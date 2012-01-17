c3df0dfe40d5a431bb69669546e32b27ffa4fe1c
aur1mas <aur1mas@devnet.lt>
2012-01-17 12:17:59 +0200

    Links API: added LinkCols & TargetCols. Refactored getLinks() - to handle more options

--- php/complete/service/LinksService.php
+++ php/complete/service/LinksService.php
@@ -17,51 +17,61 @@ class LinksService
 	{
 		$this->authenticator = $authenticator;		
 	}
-	
-	/**
-	 * This method returns a set of links to a page or domain.
-	 * 
-	 * @param objectURL
-	 * @param scope determines the scope of the Target link, as well as the Source results.
-	 * @param filters  filters the links returned to only include links of the specified type.  You may include one or more of the following values separated by '+'
-	 * @param sort determines the sorting of the links, in combination with limit and offset, this allows fast access to the top links by several orders:
-	 * @param sourceCol specifies data about the source of the link is included
-	 * @param offset The start record of the page can be specified using the Offset parameter
-	 * @param limit The size of the page can by specified using the Limit parameter.
-	 * @return
-	 */
-	public function getLinks($objectURL, $scope = null, $filters = null, $sort = null, $sourceCol = 0, $offset = -1, $limit = -1)
-	{
-		$urlToFetch = "http://lsapi.seomoz.com/linkscape/links/" . urlencode($objectURL) . "?" . $this->authenticator->getAuthenticationStr();
-		
-		if($scope != null)
-		{
-			$urlToFetch = $urlToFetch . "&Scope=" . $scope;
-		}
-		if($filters != null)
-		{
-			$urlToFetch = $urlToFetch . "&Filter=" . $filters;
-		}
-		if($sort != null)
-		{
-			$urlToFetch = $urlToFetch . "&Sort=" . $sort;
-		}
-		if($sourceCol > 0)
-		{
-			$urlToFetch = $urlToFetch . "&SourceCols=" . $sourceCol;
-		}
-		if($offset >= 0)
-		{
-			$urlToFetch = $urlToFetch . "&Offset=" . $offset;
-		}
-		if($limit >= 0)
-		{
-			$urlToFetch = $urlToFetch . "&Limit=" . $limit;
-		}
-		$response = ConnectionUtil::makeRequest($urlToFetch);
-		
-		return $response;
-	}
+    
+    /**
+     * This method returns a set of links to a page or domain.
+     *
+     * @param string $objectURL 
+     * @param array $options    options which can be passed to make request
+     * @return stdObject
+     * @author aur1mas <aur1mas@devnet.lt>
+     */
+    public function getLinks($objectURL, array $options = array())
+    {
+        $urlToFetch = "http://lsapi.seomoz.com/linkscape/links/" . urlencode($objectURL) . "?" . $this->authenticator->getAuthenticationStr();
+        
+        // scope determines the scope of the Target link, as well as the Source results
+        if (isset($options['scope'])) {
+            $urlToFetch .= "&Scope=" . $options['scope'];
+        }
+        
+        // filters the links returned to only include links of the specified type.  You may include one or more of the following values separated by '+'
+        if (isset($options['filters'])) {
+            $urlToFetch .= "&Filter=" . $options['filters'];
+        }
+        
+        // sort determines the sorting of the links, in combination with limit and offset, this allows fast access to the top links by several orders.
+        if (isset($options['sort'])) {
+            $urlToFetch .= "&Sort=" . $options['sort'];
+        }
+        
+        // specifies data about the source of the link is included
+        if (isset($options['source_cols'])) {
+            $urlToFetch .= "&SourceCols=" . $options['source_cols'];
+        }
+        
+        // specifies data about the target of the link is included
+        if (isset($options['target_cols'])) {
+            $urlToFetch .= "&TargetCols=" . $options['target_cols'];
+        }
+        
+        // specifies data about the link itself (e.g. if the link is nofollowed)
+        if (isset($options['link_cols'])) {
+            $urlToFetch .= "&LinkCols=" . $options['link_cols'];
+        }
+        
+        // The start record of the page can be specified using the Offset parameter
+        if (isset($options['offset']) && (int)$options['offset'] >= 0) {
+            $urlToFetch .= "&Offset=" . (int)$options['offset'];
+        }
+        
+        // The size of the page can by specified using the Limit parameter.
+        if (isset($options['limit']) && (int)$options['limit'] >= 0) {
+            $urlToFetch .= "&Limit=" . (int)$options['limit'];
+        }
+        
+		return ConnectionUtil::makeRequest($urlToFetch);
+    }
 	
 	/**
 	 * @return the $authenticator
