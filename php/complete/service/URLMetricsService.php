51b68ef03c489edffaf9bb5eaacfb00121b554da
aur1mas <aur1mas@devnet.lt>
2012-02-16 14:13:57 +0200

    batch requests

--- php/complete/service/URLMetricsService.php
+++ php/complete/service/URLMetricsService.php
@@ -16,14 +16,23 @@ class URLMetricsService extends AbstractService
 	 * 
 	 * This method returns the metrics about a URL or set of URLs.  
 	 * 
-	 * @param string objectURL
+	 * @param mixed <array|string> objectURL    if array passed batch request will be performed
 	 * @param array options
 	 * @return
 	 */
 	public function getUrlMetrics($objectURL, array $options = array())
 	{
+		$urlToFetch = "http://lsapi.seomoz.com/linkscape/url-metrics/";
+		$postParams = "";
 		
-		$urlToFetch = "http://lsapi.seomoz.com/linkscape/url-metrics/" . urlencode($objectURL) . "?" . $this->getAuthenticator()->getAuthenticationStr();
+		// if passed more then 1 url - pass them through post
+		if (!is_array($objectURL)) {
+		    $urlToFetch .= urlencode($objectURL);
+		} else {
+		    $postParams = json_encode($objectURL);
+		}
+		
+		$urlToFetch .= "?" . $this->getAuthenticator()->getAuthenticationStr();
         
         /**
          * @param col This field filters the data to get only specific columns
@@ -32,8 +41,8 @@ class URLMetricsService extends AbstractService
         if (isset($options['cols']) && (int)$options['cols'] > 0) {
             $urlToFetch .= "&Cols=" . $options['cols'];
         }
-        
-		$response = ConnectionUtil::makeRequest($urlToFetch);
+        var_dump($urlToFetch);
+		$response = ConnectionUtil::makeRequest($urlToFetch, $postParams);
 		
 		return $response;
 	}	
