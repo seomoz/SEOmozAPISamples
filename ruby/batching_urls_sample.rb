2e29e16153ccafc9bfc7de6c218ccb7c3838eb31
Lisa - Moz <lisa@moz.com>
2015-09-29 13:06:56 -0700

    Updated for consistency

--- ruby/batching_urls_sample.rb
+++ ruby/batching_urls_sample.rb
@@ -5,28 +5,28 @@ require 'json'
 require 'net/http'
 require 'uri'
 
-# You can obtain you access id and secret key here: http://moz.com/products/api/keys
+# Get your access id and secret key here: http://moz.com/products/api/keys
 ACCESS_ID	= "ACCESS_ID_HERE"
 SECRET_KEY	= "SECRET_KEY_HERE"
 
-# Set your expires for several minutes into the future.
-# Values excessively far in the future will not be honored by the Mozscape API.
+# Set your expires time for several minutes into the future.
+# Expires times excessively far in the future will not be honored by the Mozscape API.
 expires	= Time.now.to_i + 300
 
-# A new linefeed is necessary between your AccessID and Expires.
+# Put each parameter on a new line.
 string_to_sign = "#{ACCESS_ID}\n#{expires}"
 
 # Get the "raw" or binary output of the hmac hash.
 binary_signature = OpenSSL::HMAC.digest('sha1', SECRET_KEY, string_to_sign)
 
-# We need to base64-encode it and then url-encode that.
+# Base64-encode it and then url-encode that.
 URL_SAFE_SIGNATURE = CGI::escape(Base64.encode64(binary_signature).chomp)
 
-# This is the URL that we want metrics for.
+# Specify the URL that you want metrics for.
 object_url = 'www.moz.com'
 
 # Add up all the bit flags you want returned.
-# Learn more here: http://apiwiki.moz.com/query-parameters/
+# Learn more here: https://moz.com/help/guides/moz-api/mozscape/api-reference/url-metrics
 cols = '103079215108'
 
 # Now put your entire request together.
