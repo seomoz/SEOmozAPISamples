c04d94dab12259116c97b7c6f2d2c5fca2ac4a8b
Lisa - Moz <lisa@moz.com>
2015-09-29 12:13:05 -0700

    SEOmoz.org to moz.com

--- python/example.py
+++ python/example.py
@@ -9,21 +9,21 @@ l = lsapi('my-access-id', 'my-secret-key')
 # are a lot, and the names are long. So, the API calls have
 # defaults
 
-# Let's get some urlmetrics. Results are now an array of dictionaries
+# Let's get some URL metrics. Results are now an array of dictionaries
 # the i'th dictionary is the results for the i'th URL
-metrics = l.urlMetrics(['www.seomoz.org', 'www.seomoz.org/blog'])
+metrics = l.urlMetrics(['www.moz.com', 'www.moz.com/blog'])
 # Now let's say we only want specific columns in the results
-authorities = l.urlMetrics(['www.seomoz.org'], lsapi.UMCols.domainAuthority | lsapi.UMCols.pageAuthority)
+authorities = l.urlMetrics(['www.moz.com'], lsapi.UMCols.domainAuthority | lsapi.UMCols.pageAuthority)
 # Or if you just need results for one URL
-mozMetrics = l.urlMetrics('www.seomoz.org')
+mozMetrics = l.urlMetrics('www.moz.com')
 
 # Now for some anchor text results
-anchorResults = l.anchorText('www.seomoz.org/blog')
+anchorResults = l.anchorText('www.moz.com/blog')
 # Or for just specific columns
-anchorTermResults = l.anchorText('www.seomoz.org/blog', cols=lsapi.ATCols.term)
+anchorTermResults = l.anchorText('www.moz.com/blog', cols=lsapi.ATCols.term)
 
 # Now for some links results
-links = l.links('www.seomoz.org')
+links = l.links('www.moz.com')
 # The links API has more columns to specify, as well as sort, scope, etc.
-links = l.links('www.seomoz.org', scope='domain_to_domain', sort='domain_authority',
+links = l.links('www.moz.com', scope='domain_to_domain', sort='domain_authority',
 	filters=['external', 'nofollow'], targetCols = lsapi.UMCols.url)
