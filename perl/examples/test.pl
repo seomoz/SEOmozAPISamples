f2620aec694361f03415b71411c1c87e950a92dc
Brandon Forehand <brandon@moz.com>
2016-05-25 10:45:23 -0700

    Rename WebService-SEOmoz-API to WebService-Mozscape-API

--- perl/examples/test.pl
+++ perl/examples/test.pl
@@ -4,25 +4,25 @@ use strict;
 use warnings;
 use FindBin qw/$Bin/;
 use lib "$Bin/../lib";
-use WebService::SEOmoz::API;
+use WebService::Mozscape::API;
 use Data::Dumper;
 
-my $seomoz = WebService::SEOmoz::API->new(
-    accessID   => $ENV{SEOmoz_ID},
-    secretKey  => $ENV{SEOmoz_KEY},
-) or die "Can't init the seomoz instance: " . $WebService::SEOmoz::API::errstr;
+my $mozscape = WebService::Mozscape::API->new(
+    accessID   => $ENV{MOZSCAPE_ACCESS_ID},
+    secretKey  => $ENV{MOZSCAPE_SECRET_KEY},
+) or die "Can't init the mozscape instance: " . $WebService::Mozscape::API::errstr;
 
-my $t = $seomoz->getUrlMetrics( {
-    objectURL => 'www.seomoz.org/blog',
-} ) or die $seomoz->errstr;
+my $t = $mozscape->getUrlMetrics( {
+    objectURL => 'moz.com/blog',
+} ) or die $mozscape->errstr;
 print Dumper(\$t);
 
-$t = $seomoz->getLinks( {
+$t = $mozscape->getLinks( {
     objectURL => 'www.google.com',
     Scope => 'page_to_page',
     Sort  => 'page_authority',
     Limit => 1,
-} ) or die $seomoz->errstr;
+} ) or die $mozscape->errstr;
 print Dumper(\$t);
 
 1;
